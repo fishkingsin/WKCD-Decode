3f856ea4b59a334badd2d744e81f21b740e3da73
James Kong <fishkingsin@gmail.com>
2011-01-11 12:53:32 +0800

    update GPL license
    Signed-off-by: James Kong <fishkingsin@gmail.com>

--- src/ARHelper.h
+++ src/ARHelper.h
@@ -1,5 +1,20 @@
+/*
+<Interactive installtion for Hong Kong WKCD exhibition>
+    Copyright (C) <2009>  <James Kong>
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #define OF_ADDON_USING_OFXOPENCV
 #define OF_ADDON_USING_OFXOBJLOADER
 #define OF_ADDON_USING_OFXDIRLIST
